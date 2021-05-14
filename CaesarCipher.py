66e4b0113970659d4dfb11f39277de0ceac4a3d6
hazardgoat <74040471+hazardgoat@users.noreply.github.com>
2021-05-14 10:23:54 -0700

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -6,7 +6,7 @@ lowerCase = { 1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', 10:
 upperCase = {1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 10:'J', 11:'K',
         12:'L', 13:'M', 14:'N', 15:'O', 16:'P', 17:'Q', 18:'R', 19:'S', 20:'T', 21:'U', 
         22:'V', 23:'W', 24:'X', 25:'Y', 26:'Z'}
-#dictionary that passes the lowerCase and upperCase dictionaries into the translation functions
+#dictionary that passes the lowerCase and upperCase dictionaries into the translation function
 dic = {}
 #dictionary that holds the translated lowerCase and upperCase dictionaries for a given shift value
 cipherDict = {}
