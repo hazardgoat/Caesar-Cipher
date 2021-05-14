6bd350db2b5a76a3648b8d86283ceedf56b0db7a
hazardgoat <74040471+hazardgoat@users.noreply.github.com>
2021-05-14 10:32:18 -0700

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,4 +1,4 @@
-#This program allows the user to encode or decode a Caesar cipher shifted by a given degree
+#This program allows the user to encode or decode a Caesar cipher right-shifted by a given degree
 
 lowerCase = { 1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', 10:'j', 11:'k',
         12:'l', 13:'m', 14:'n', 15:'o', 16:'p', 17:'q', 18:'r', 19:'s', 20:'t', 21:'u', 
